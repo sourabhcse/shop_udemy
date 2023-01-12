c2fba7d215644c6b1531107475e26fe80ebb1865
sourabhcse <sourabhsaini201098>
2023-01-12 22:56:06 +0530

    add data

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql= require('mysql2');
+const Sequelize=require('sequelize');
 
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'210431@rath'
+const sequelize=new Sequelize('node-complete','root','210431@rath',{
+    dailect:'mysql',
+    host:'localhost'
 });
 
-module.exports=pool.promise();
+module.exports=sequelize;
